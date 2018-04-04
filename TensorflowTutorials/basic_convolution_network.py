8b75551a5e8614f538f714ea52eeefaee3f7d53a
Jady <taiyi.pei@gmail.com>
2018-04-04 13:19:58 +0800

    添加基本的神经网络列子

---
+++ TensorflowTutorials/basic_convolution_network.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#一个简单的有2个隐藏层的神经网络： L1(Input, 128组图片) -> L2(784, 625) -> L3(625,625) -> L4(625,10) -> output(none, 10)
+
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+def init_weights(shape):
+    return tf.Variable(tf.random_normal(shape, stddev=0.01))
+
+
+def model(X, w_h, w_h2, w_o, p_keep_input, p_keep_hidden): # this network is the same as the previous one except with an extra hidden layer + dropout
+    X = tf.nn.dropout(X, p_keep_input) # 保留input的80%的元素，784*0.8， 防止过拟合(train loss < test loss)
+    h = tf.nn.relu(tf.matmul(X, w_h)) # ReLU： Rectified Linear Unit 整流线性单元， y(x) = 0 if x < 0, y(x) = x if x >= 0
+                                      # 相当于虑调了小于0的那部分，相较sigmoid和tanh,减小梯度消失的发生
+
+    h = tf.nn.dropout(h, p_keep_hidden)
+    h2 = tf.nn.relu(tf.matmul(h, w_h2))
+
+    h2 = tf.nn.dropout(h2, p_keep_hidden) # 保留50%
+
+    return tf.matmul(h2, w_o)
+
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
+
+X = tf.placeholder("float", [None, 784])
+Y = tf.placeholder("float", [None, 10])
+
+w_h = init_weights([784, 625])
+w_h2 = init_weights([625, 625])
+w_o = init_weights([625, 10])
+
+p_keep_input = tf.placeholder("float")
+p_keep_hidden = tf.placeholder("float")
+py_x = model(X, w_h, w_h2, w_o, p_keep_input, p_keep_hidden)
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y))
+train_op = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost) # RMS(Root Mean Square, 均方根) Prop 依赖于全局学习速率，适合处理平稳目标，尤其RNN
+predict_op = tf.argmax(py_x, 1)
+
+# Launch the graph in a session
+with tf.Session() as sess:
+    # you need to initialize all variables
+    tf.global_variables_initializer().run()
+
+    for i in range(100):
+        for start, end in zip(range(0, len(trX), 128), range(128, len(trX)+1, 128)):
+            sess.run(train_op, feed_dict={X: trX[start:end], Y: trY[start:end],
+                                          p_keep_input: 0.8, p_keep_hidden: 0.5}) # 训练，计算model中的w_h, w_h2, w_o
+        print(i, np.mean(np.argmax(teY, axis=1) ==
+                         sess.run(predict_op, feed_dict={X: teX, 
+                                                         p_keep_input: 1.0,
+                                                         p_keep_hidden: 1.0}))) # 预测, 基于训练好的w_h, w_h2, w_o
